4b0ff34374b8afad52e5b8b5dd3bbdba59ab320e
sylvianeoliveira <sylvianesno@dcc.ufrj.br>
2022-01-28 01:09:57 -0300

    Construção do chat offline

--- pages/chat.js
+++ pages/chat.js
@@ -1,5 +1,214 @@
-export default function ChatsPage(){
-    return(
-        <div>Chats Page</div>
+import { Box, Text, TextField, Image, Button } from '@skynexui/components';
+import React from 'react';
+import appConfig from '../config.json';
+
+
+export default function ChatPage() {
+const [mensagem, setMensagem] = React.useState('');
+const [listaDeMensagens, setListaDeMensagens] = React.useState([]);
+
+
+function handleNovaMensagem(novaMensagem) {
+    const mensagem = {
+        id: listaDeMensagens.length + 1,
+        de: 'sylvianeoliveira',
+        texto: novaMensagem,
+    };
+
+    setListaDeMensagens([
+        mensagem,
+        ...listaDeMensagens,
+    ]);
+    setMensagem('');
+}
+
+    return (
+        <Box
+            styleSheet={{
+                display: 'flex', alignItems: 'center', justifyContent: 'center',
+                backgroundImage: `url(https://i.ibb.co/ZmqNCwh/fundo.png)`,
+                backgroundRepeat: 'no-repeat', backgroundSize: 'cover', backgroundBlendMode: 'multiply',
+            }}
+        >
+            <Box
+                styleSheet={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    flex: 1,
+                    boxShadow: '0 2px 10px 0 rgb(0 0 0 / 20%)',
+                    borderRadius: '5px',
+                    backgroundColor: appConfig.theme.colors.neutrals[900],
+                    height: '100%',
+                    maxWidth: '95%',
+                    maxHeight: '95vh',
+                    padding: '32px',
+                }}
+            >
+                <Header />
+                <Box
+                    styleSheet={{
+                        position: 'relative',
+                        display: 'flex',
+                        flex: 1,
+                        height: '80%',
+                        backgroundColor: appConfig.theme.colors.neutrals[800],
+                        flexDirection: 'column',
+                        borderRadius: '5px',
+                        padding: '16px',
+                    }}
+                >
+                    <MessageList mensagens={listaDeMensagens} />
+
+                    <Box
+                        as="form"
+                        onSubmit={function (event) {
+                            event.preventDefault();
+                            handleNovaMensagem(mensagem);
+                        }}
+
+                        styleSheet={{
+                            display: 'flex',
+                            alignItems: 'center',
+                        }}
+                    >
+                        <TextField
+                            value={mensagem}
+                            onChange={(event) => {
+                                const valor = event.target.value;
+                                setMensagem(valor);
+                            }}
+                            onKeyPress={(event) => {
+                                if (event.key === 'Enter' ) {
+                                    event.preventDefault();
+                                    handleNovaMensagem(mensagem);
+                                }
+                            }}
+
+                            placeholder="Insira sua mensagem aqui..."
+                            type="textarea"
+                            styleSheet={{
+                                width: '100%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '15px 8px',
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                                marginRight: '12px',
+                                color: appConfig.theme.colors.neutrals[200],
+                            }}
+                        />
+                        
+                        <Button
+                            type='submit'
+                            label='Enviar'
+                            styleSheet={{
+                                width: '7%',
+                                border: '0',
+                                resize: 'none',
+                                borderRadius: '5px',
+                                padding: '15px 8px',
+                                backgroundColor: appConfig.theme.colors.primary[900],
+                                marginRight: '12px',
+                                marginBottom: '7px',
+                                color: appConfig.theme.colors.neutrals[900],
+                            }}
+
+                            buttonColors={{
+                                contrastColor: appConfig.theme.colors.neutrals["900"],
+                                mainColor: appConfig.theme.colors.primary[900],
+                                mainColorLight: appConfig.theme.colors.primary[800],
+                                mainColorStrong: appConfig.theme.colors.primary[700],
+                            }}
+                        />
+                    </Box>
+                </Box>
+            </Box>
+        </Box>
+    )
+}
+
+function Header() {
+    return (
+        <>
+            <Box styleSheet={{ width: '100%', marginBottom: '16px', display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} >
+                <Text variant='heading4'
+                styleSheet={{
+                        color: appConfig.theme.colors.primary[800],
+                        
+                        }}
+                >
+                    Star Wars Chat
+                </Text>
+                <Button
+                    variant='tertiary'
+                    colorVariant='neutral'
+                    label='Logout'
+                    href="/"
+                />
+            </Box>
+        </>
+    )
+}
+function MessageList(props) {
+    return (
+        <Box
+            tag="ul"
+            styleSheet={{
+                overflow: 'scroll',
+                display: 'flex',
+                flexDirection: 'column-reverse',
+                flex: 1,
+                color: appConfig.theme.colors.neutrals["000"],
+                marginBottom: '16px',
+            }}
+        >
+            {props.mensagens.map((mensagem) => {
+                return (
+                    <Text
+                        key={mensagem.id}
+                        tag="li"
+                        styleSheet={{
+                            borderRadius: '5px',
+                            padding: '6px',
+                            marginBottom: '12px',
+                            hover: {
+                                backgroundColor: appConfig.theme.colors.neutrals[700],
+                            }
+                        }}
+                    >
+                        <Box
+                            styleSheet={{
+                                marginBottom: '8px',
+                            }}
+                        >
+                            <Image
+                                styleSheet={{
+                                    width: '20px',
+                                    height: '20px',
+                                    borderRadius: '50%',
+                                    display: 'inline-block',
+                                    marginRight: '8px',
+                                }}
+                                src={`https://github.com/sylvianeoliveira.png`}
+                            />
+                            <Text tag="strong">
+                                {mensagem.de}
+                            </Text>
+                            <Text
+                                styleSheet={{
+                                    fontSize: '10px',
+                                    marginLeft: '8px',
+                                    color: appConfig.theme.colors.neutrals[300],
+                                }}
+                                tag="span"
+                            >
+                                {(new Date().toLocaleDateString())}
+                            </Text>
+                        </Box>
+                        {mensagem.texto}
+                    </Text>
+                );
+            })}
+        </Box>
     )
 }
